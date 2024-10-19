b6794fcc611ac8c26299a0b352b1a540a7cce60e
Erick Onguka <79970016+erickonguka@users.noreply.github.com>
2024-10-19 08:27:10 +0300

    Agent name, company name

--- core/config.py
+++ core/config.py
@@ -3,3 +3,5 @@ import google.generativeai as genai
 genai.configure(api_key=os.environ["AI_API_KEY"])
 
 bearerToken = "your_bearer token"
+company_name="your company name"
+agent_name="bot agent name"
