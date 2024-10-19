5b16881423203ec005b92e35ee51200dcdcd75da
Erick Onguka <79970016+erickonguka@users.noreply.github.com>
2024-10-19 08:41:03 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,22 @@
 import os
 import google.generativeai as genai
+
+# Gemini API KEY
 genai.configure(api_key=os.environ["AI_API_KEY"])
 
+# Facebook/WhatsApp Access Bearer Token
 bearerToken = "your_bearer token"
+
+# Fill here too
 company_name="your company name"
 agent_name="bot agent name"
+currency="your currency"
+
+# Use a more robust data storing mechanism like a database
+base_dir = os.path.dirname(__file__)
+
+# path to your training data/info about your company
+training_data = os.path.join(base_dir, 'imports', 'training_data.txt') 
+
+# path to whatsapp formatting options
+formatting = os.path.join(base_dir, 'imports', 'formatting.txt')   
